ac8d8826a1d4c0bc110cfa974126ea791861a78e
rajeshtechstack <rajeshb7631@gmail.com>
2022-03-13 19:34:10 +0530

    Created Service interfaces

---
+++ pet-clinic-data/src/main/java/com/example/springpetclinic/service/VetService.java
@@ -0,0 +1,11 @@
+package com.example.springpetclinic.service;
+
+import com.example.springpetclinic.model.Vet;
+
+import java.util.Set;
+
+public interface VetService {
+    Vet findById(Long id);
+    Vet save(Vet vet);
+    Set<Vet> findAll();
+}
