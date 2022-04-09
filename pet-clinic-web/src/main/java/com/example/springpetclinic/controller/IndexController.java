3766b90f4325a141afe23711a15546cdcd89eb88
rajeshtechstack <rajeshb7631@gmail.com>
2022-04-09 19:38:47 +0530

    logback-spring.xml file added to find logs

--- pet-clinic-web/src/main/java/com/example/springpetclinic/controller/IndexController.java
+++ pet-clinic-web/src/main/java/com/example/springpetclinic/controller/IndexController.java
@@ -1,5 +1,7 @@
 package com.example.springpetclinic.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,19 +10,24 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping(value = "/api/v1")
 public class IndexController {
 
+    private final static Logger logger = LoggerFactory.getLogger(IndexController.class);
+
     @RequestMapping({"","/","index", "index.html"})
     public String getIndexPage(Model model){
+        logger.info("IndexController:: getIndexPage:");
         model.addAttribute("welcome","Welcome to Index page");
         return "index";
     }
     @RequestMapping({"/notimplemented", "/notimplemented.html"})
     public String getNotImplementedPage(Model model){
+        logger.info("IndexController:: getNotImplementedPage: ");
         model.addAttribute("notimplemented","Not Implemented yet");
         return "notimplemented";
     }
 
     @RequestMapping({"/oups"})
     public String getOopsPage(Model model){
+        logger.info("IndexController:: getOopsPage: ");
         model.addAttribute("notimplemented","Not Implemented yet");
         return "notimplemented";
     }
--- pet-clinic-web/src/main/java/com/example/springpetclinic/controller/PetController.java
+++ pet-clinic-web/src/main/java/com/example/springpetclinic/controller/PetController.java
@@ -2,12 +2,17 @@ package com.example.springpetclinic.controller;
 
 import com.example.springpetclinic.service.PetService;
 import com.example.springpetclinic.service.PetServiceEX;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/api/v1")
 public class PetController {
+
+    private final static Logger logger = LoggerFactory.getLogger(PetController.class);
+
     private final PetServiceEX petServiceEX;
 
     public PetController(PetServiceEX petServiceEX) {
@@ -15,6 +20,7 @@ public class PetController {
     }
 
     public String whichPetIsTheBest(){
+        logger.info("PetController:: whichPetIsTheBest: {}",petServiceEX.getPetType());
         return petServiceEX.getPetType();
     }
 }
