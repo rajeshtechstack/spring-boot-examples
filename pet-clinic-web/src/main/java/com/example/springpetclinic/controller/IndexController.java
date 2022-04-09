6c1fd4d5a312e87da09d8971e11724ac42f7a1e5
rajeshtechstack <rajeshb7631@gmail.com>
2022-04-09 23:05:11 +0530

    SwaggerConfiguration added

--- pet-clinic-web/src/main/java/com/example/springpetclinic/controller/IndexController.java
+++ pet-clinic-web/src/main/java/com/example/springpetclinic/controller/IndexController.java
@@ -5,6 +5,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 @RequestMapping(value = "/api/v1")
@@ -12,20 +13,20 @@ public class IndexController {
 
     private final static Logger logger = LoggerFactory.getLogger(IndexController.class);
 
-    @RequestMapping({"","/","index", "index.html"})
+    @RequestMapping(value = { "/index.html"}, method = RequestMethod.GET)
     public String getIndexPage(Model model){
         logger.info("IndexController:: getIndexPage:");
         model.addAttribute("welcome","Welcome to Index page");
         return "index";
     }
-    @RequestMapping({"/notimplemented", "/notimplemented.html"})
+    @RequestMapping(value = {"/notimplemented", "/notimplemented.html"}, method = RequestMethod.GET)
     public String getNotImplementedPage(Model model){
         logger.info("IndexController:: getNotImplementedPage: ");
         model.addAttribute("notimplemented","Not Implemented yet");
         return "notimplemented";
     }
 
-    @RequestMapping({"/oups"})
+    @RequestMapping(value = {"/oups"}, method = RequestMethod.GET)
     public String getOopsPage(Model model){
         logger.info("IndexController:: getOopsPage: ");
         model.addAttribute("notimplemented","Not Implemented yet");
